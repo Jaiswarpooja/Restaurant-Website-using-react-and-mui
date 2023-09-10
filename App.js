e26022c5d19eb75666e94eb3ca044bc6f427cbaa
Jaiswarpooja <89985685+Jaiswarpooja@users.noreply.github.com>
2023-09-10 22:28:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import {BrowserRouter, Routes , Route } from 'react-router-dom';
+import Home from './pages/Home'
+import Pagenotfound from './pages/Pagenotfound';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Menu from './pages/Menu';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route  path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route  path='/menu' element={<Menu/>}/>
+        <Route path='*' element={<Pagenotfound/>} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+      
+  );
+}
+
+export default App;
